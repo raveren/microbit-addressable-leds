317bc5f2107024824c79cae948b15e262daca2fc
raveren <raveren@gmail.com>
2019-10-20 13:22:09 +0300

    tested memory limits further, more complex

--- array_size_test.ts
+++ array_size_test.ts
@@ -1,20 +1,64 @@
-let numPixels = 64
+let numPixels = 256
 
 let pixelAnimations: number[][][] = []
+let pixelColors: number[][] = []
 
 for (let i = 0; i < numPixels; i++) {
-    basic.showNumber(i) // this goes up to 30 before out of memory
+    basic.showNumber(i)
 
-    pixelAnimations[i] = [
-        [6,7],
-        [6,7],
-        [6,7],
-        [6,7],
-        [6,7],
-        [6,7],
-        [6,7],
+    pixelColors[i] = [ // it now goes to ~110+
+        /*colorNowR:*/ 0,
+        /*colorNowG:*/ 0,
+        /*colorNowB:*/ 0,
+
+        /*colorToR:*/ 235,
+        /*colorToR:*/ 235,
+        /*colorToR:*/ 0,
+
+        /*framesFor transition:*/ 0,
     ]
+    /*
+        pixelAnimations[i] = [
+            [6,7],
+            [6,7],
+            [6,7],
+            [6,7],
+            [6,7],
+            [6,7],
+            [6,7],
+        ]
+    */
+
+
+    /**
+     // real code
+     pixelAnimations[i] = [
+     {
+                color: neopixel.rgb(50, 50, 0),
+                frames: 3
+            },
+     {
+                color: neopixel.rgb(120, 120, 0),
+                frames: 3
+            },
+     {
+                color: neopixel.rgb(235, 235, 0),
+                frames: 8
+            },
+     {
+                color: neopixel.rgb(180, 180, 0),
+                frames: 3
+            },
+     {
+                color: neopixel.rgb(50, 50, 0),
+                frames: 3
+            },
+     {
+                color: neopixel.rgb(0, 0, 0),
+                frames: 0
+            }
+     ]
+
+     */
 
 }
-
-
